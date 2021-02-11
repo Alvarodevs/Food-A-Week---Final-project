bcee9eda7159b5533cc48f7b87284c00681175b0
nitlara <nitlara@gmail.com>
2021-02-11 15:38:49 +0000

    footer image is visible in some screens

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,9 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => {
 	return (
 		<footer className="footer mt-auto py-3 text-center">
-			<div className="background_footer">
-				<p className="year">2021</p>
-			</div>
+			<div className="background_footer">2021</div>
 		</footer>
 	);
 };
