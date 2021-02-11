ba1ea845e3657261e1cbb02bcf499b12d9a2baa7
nitlara <nitlara@gmail.com>
2021-02-11 15:57:08 +0000

    navbar changes tipografia

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 export const Footer = () => {
 	return (
 		<footer className="footer mt-auto py-3 text-center">
-			<div className="background_footer">2021</div>
+			<div className="background_footer d-flex align-text-bottom justify-content-center mt-5 ">
+				<div className="align-self-end">2021 Food A Week</div>
+			</div>
 		</footer>
 	);
 };
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../img/containers.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="d-flex  navbar navbar-light background-pink mb-1">
+		<nav className="d-flex justify-content-around navbar navbar-light background-pink mb-1">
 			<Link to="/">
 				{/*<span className="navbar-brand mb-0 h1">Home ICON</span> */}
-				<img
+				{/* <img
 					src="https://image.flaticon.com/icons/png/512/1676/1676708.png"
 					className="d-flex navbar-brand mb-0 justify-content-auto"
-				/>
+				/> */}
+				<img src={logo} alt="Logo" className="d-flex navbar-brand mb-0  ml-5" />;
 			</Link>
 			<div className="text-center mt-3 mx-auto">
 				<h2>{"Choose your recipes, plan your week"}</h2>
