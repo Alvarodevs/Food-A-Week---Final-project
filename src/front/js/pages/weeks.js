f8706f0935931b0c4de82742e960e372f6c39f9a
nitlara <nitlara@gmail.com>
2021-02-15 20:28:56 +0000

    weeks two rows

--- src/front/js/pages/weeks.js
+++ src/front/js/pages/weeks.js
@@ -15,69 +15,122 @@ export const AllWeeks = () => {
 				<h1>{"All your weeks"}</h1>
 			</div>
 			<div>
-				<div className="card-deck justify-content-around mt-3">
-					<div className="card  menuWeek justify-content-center p-0 m-0">
-						<img
-							className="card-img-top p-0 m-0 w-75 h-75"
-							src="https://static01.nyt.com/images/2020/01/24/dining/yk-gochujang-chicken-and-vegetables/merlin_167664060_7435c624-7225-4cb1-b104-4d67761185a4-articleLarge.jpg"
-							alt="Card image cap"
-						/>
+				<div className="card-container d-flex justify-content-center">
+					<div className="row">
+						<div className="card col-3 menuWeek p-0 m-0 mr-4 mb-4">
+							<img
+								className="card-img-top p-0 m-0"
+								src="https://static01.nyt.com/images/2020/01/24/dining/yk-gochujang-chicken-and-vegetables/merlin_167664060_7435c624-7225-4cb1-b104-4d67761185a4-articleLarge.jpg"
+								alt="Card image cap"
+							/>
+							<div className="d-flex card-body py-1 justify-content-between align-middle">
+								<div className="card-title pt-2">Semana sin pescado</div>
+								<div className="d-flex">
+									<Button className="p-1 weekplan-btn btn green-button" type="submit">
+										<Link to="/weekjumbotron">Show</Link>
+									</Button>
+									<Button className="p-1 weekplan-btn btn green-button" type="submit">
+										Edit
+									</Button>
+								</div>
+							</div>
+						</div>
+						<div className="card col-3 menuWeek p-0 m-0 mr-4 mb-4">
+							<img
+								className="card-img-top p-0 m-0"
+								src="https://static01.nyt.com/images/2020/01/24/dining/yk-gochujang-chicken-and-vegetables/merlin_167664060_7435c624-7225-4cb1-b104-4d67761185a4-articleLarge.jpg"
+								alt="Card image cap"
+							/>
+							<div className="d-flex card-body py-1 justify-content-between align-middle">
+								<div className="card-title pt-2">Semana sin pescado</div>
+								<div className="d-flex">
+									<Button className="p-1 weekplan-btn btn green-button" type="submit">
+										<Link to="/weekjumbotron">Show</Link>
+									</Button>
+									<Button className="p-1 weekplan-btn btn green-button" type="submit">
+										Edit
+									</Button>
+								</div>
+							</div>
+						</div>
+						<div className="card col-3 menuWeek p-0 m-0 mr-4 mb-4">
+							<img
+								className="card-img-top p-0 m-0"
+								src="https://static01.nyt.com/images/2020/01/24/dining/yk-gochujang-chicken-and-vegetables/merlin_167664060_7435c624-7225-4cb1-b104-4d67761185a4-articleLarge.jpg"
+								alt="Card image cap"
+							/>
+							<div className="d-flex card-body py-1 justify-content-between align-middle">
+								<div className="card-title pt-2">Semana sin pescado</div>
+								<div className="d-flex">
+									<Button className="p-1 weekplan-btn btn green-button" type="submit">
+										<Link to="/weekjumbotron">Show</Link>
+									</Button>
+									<Button className="p-1 weekplan-btn btn green-button" type="submit">
+										Edit
+									</Button>
+								</div>
+							</div>
+						</div>
 
-						<div className="d-flex card-body mt-2 py-1 justify-content-between align-middle">
-							<div className="card-title text-center justify-content-center pt-2">Semana sin pescado</div>
-							<div className="d-flex">
-								<Button className="p-1 weekplan-btn btn green-button" type="submit">
-									<Link to="/weekjumbotron">Show</Link>
-								</Button>
-								<Button className="p-1 weekplan-btn btn green-button" type="submit">
-									Edit
-								</Button>
+						<div className="row">
+							<div className="card col-3 menuWeek p-0 m-0 mr-4 mb-4">
+								<img
+									className="card-img-top p-0 m-0"
+									src="https://static01.nyt.com/images/2020/01/24/dining/yk-gochujang-chicken-and-vegetables/merlin_167664060_7435c624-7225-4cb1-b104-4d67761185a4-articleLarge.jpg"
+									alt="Card image cap"
+								/>
+								<div className="d-flex card-body py-1 justify-content-between align-middle">
+									<div className="card-title pt-2">Semana sin pescado</div>
+									<div className="d-flex">
+										<Button className="p-1 weekplan-btn btn green-button" type="submit">
+											<Link to="/weekjumbotron">Show</Link>
+										</Button>
+										<Button className="p-1 weekplan-btn btn green-button" type="submit">
+											Edit
+										</Button>
+									</div>
+								</div>
 							</div>
-						</div>
-					</div>
-					<div className="card col-4 menuWeek p-0 m-0">
-						<img
-							className="card-img-top p-0 m-0"
-							src="https://static01.nyt.com/images/2020/01/24/dining/yk-gochujang-chicken-and-vegetables/merlin_167664060_7435c624-7225-4cb1-b104-4d67761185a4-articleLarge.jpg"
-							alt="Card image cap"
-						/>
-						<div className="d-flex card-body py-1 justify-content-between align-middle">
-							<div className="card-title pt-2">Semana sin pescado</div>
-							<div className="d-flex">
-								<Button className="p-1 weekplan-btn btn green-button" type="submit">
-									<Link to="/weekjumbotron">Show</Link>
-								</Button>
-								<Button className="p-1 weekplan-btn btn green-button" type="submit">
-									Edit
-								</Button>
+							<div className="card col-3 menuWeek p-0 m-0 mr-4 mb-4">
+								<img
+									className="card-img-top p-0 m-0"
+									src="https://static01.nyt.com/images/2020/01/24/dining/yk-gochujang-chicken-and-vegetables/merlin_167664060_7435c624-7225-4cb1-b104-4d67761185a4-articleLarge.jpg"
+									alt="Card image cap"
+								/>
+								<div className="d-flex card-body py-1 justify-content-between align-middle">
+									<div className="card-title pt-2">Semana sin pescado</div>
+									<div className="d-flex">
+										<Button className="p-1 weekplan-btn btn green-button" type="submit">
+											<Link to="/weekjumbotron">Show</Link>
+										</Button>
+										<Button className="p-1 weekplan-btn btn green-button" type="submit">
+											Edit
+										</Button>
+									</div>
+								</div>
 							</div>
-						</div>
-					</div>
-					<div className="card col-4 menuWeek p-0 m-0">
-						<img
-							className="card-img-top p-0 m-0"
-							src="https://static01.nyt.com/images/2020/01/24/dining/yk-gochujang-chicken-and-vegetables/merlin_167664060_7435c624-7225-4cb1-b104-4d67761185a4-articleLarge.jpg"
-							alt="Card image cap"
-						/>
-						<div className="d-flex card-body py-1 justify-content-between align-middle">
-							<div className="card-title pt-2">Semana sin pescado</div>
-							<div className="d-flex">
-								<Button className="p-1 weekplan-btn btn green-button" type="submit">
-									<Link to="/weekjumbotron">Show</Link>
-								</Button>
-								<Button className="p-1 weekplan-btn btn green-button" type="submit">
-									Edit
-								</Button>
+							<div className="card col-3 menuWeek p-0 m-0 mr-4 mb-4">
+								<img
+									className="card-img-top p-0 m-0"
+									src="https://static01.nyt.com/images/2020/01/24/dining/yk-gochujang-chicken-and-vegetables/merlin_167664060_7435c624-7225-4cb1-b104-4d67761185a4-articleLarge.jpg"
+									alt="Card image cap"
+								/>
+								<div className="d-flex card-body py-1 justify-content-between align-middle">
+									<div className="card-title pt-2">Semana sin pescado</div>
+									<div className="d-flex">
+										<Button className="p-1 weekplan-btn btn green-button" type="submit">
+											<Link to="/weekjumbotron">Show</Link>
+										</Button>
+										<Button className="p-1 weekplan-btn btn green-button" type="submit">
+											Edit
+										</Button>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<Link to="/newweek" className="d-flex justify-content-end">
-				<Button type="submit" className="btn pink-button mt-5 ">
-					Get me out of here! I want to generate a new week
-				</Button>
-			</Link>
 		</div>
 	);
 };
