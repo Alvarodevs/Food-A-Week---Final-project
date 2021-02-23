d12e041ee0fdcf40ed8c922e14768d4708f0c488
nitlara <nitlara@gmail.com>
2021-02-23 13:46:54 +0000

    after revert

--- src/front/js/component/allergenstable.js
+++ src/front/js/component/allergenstable.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
 import { Context } from "../store/appContext";
 
 export const AllergensTable = props => {
