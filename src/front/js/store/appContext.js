c95529527d703b386e0c798ecc328055ceb0e904
nitlara <nitlara@gmail.com>
2021-02-23 13:41:38 +0000

    Revert "flux edited - external API"
    This reverts commit 1c43ea8e78c5d87a171f75d04bd9a474e2a54086.

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -28,16 +28,15 @@ const injectContext = PassedComponent => {
 			 * you should do your ajax requests or fetch api requests here. Do not use setState() to save data in the
 			 * store, instead use actions, like this:
 			 **/
-            state.actions.getMessage(); 
-            state.actions.getRecipes();// <---- calling this function from the flux.js actions
+			state.actions.getMessage(); // <---- calling this function from the flux.js actions
 		}, []);
 
-		// useEffect(
-		// 	() => {
-		// 		getRecipes();
-		// 	},
-		// 	[query]
-		// );
+		useEffect(
+			() => {
+				getRecipes();
+			},
+			[query]
+		);
 		// The initial value for the context is not null anymore, but the current state of this component,
 		// the context will now have a getStore, getActions and setStore functions available, because they were declared
 		// on the state of this component
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,21 +2,68 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			message: null,
-			recipes: []
+			demo: [
+				{
+					title: "FIRST",
+					background: "white",
+					initial: "white"
+				},
+				{
+					title: "SECOND",
+					background: "white",
+					initial: "white"
+				}
+            ],
+            recipeFullData: [
+                {
+                    label: "",
+                    image: "",
+                    ingredients:
+                        {
+                            text: "",
+                            weight: ""
+                        },
+                    totalTime: "",
+                    healthLabels: "",
+                    url: ""
+                }
+            ],
 		},
-
 		actions: {
-			getRecipes: () => {
-				const APP_ID = "89991040";
-				const APP_KEY = "8868684a401c319d7c41e1d6ec8a2870";
-				const query = "chicken";
-				const recipeCount = "";
-				var url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=${recipeCount}`;
+			getRecipes: async () => {
+				const response = await fetch(
+					`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&health=${allergens}`
+				);
+				const data = await response.json();
+				setRecipes(data.hits);
+				console.log(data.hits);
+			},
 
-				fetch(url)
+			// Use getActions to call a function within a fuction
+			exampleFunction: () => {
+				getActions().changeColor(0, "green");
+			},
+
+			getMessage: () => {
+				// fetching data from the backend
+				fetch(process.env.BACKEND_URL + "/api/hello")
 					.then(resp => resp.json())
-					.then(data => setStore({ recipes: data.results }))
+					.then(data => setStore({ message: data.message }))
 					.catch(error => console.log("Error loading message from backend", error));
+			},
+			changeColor: (index, color) => {
+				//get the store
+				const store = getStore();
+
+				//we have to loop the entire demo array to look for the respective index
+				//and change its color
+				const demo = store.demo.map((elm, i) => {
+					if (i === index) elm.background = color;
+					return elm;
+				});
+
+				//reset the global store
+				setStore({ demo: demo });
 			}
 		}
 	};
