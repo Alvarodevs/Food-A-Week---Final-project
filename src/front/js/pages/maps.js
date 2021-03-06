928e4c2db7f44ca2cfc6db84412ac6803947ec51
Alvarodevs <alvaro.develops@gmail.com>
2021-03-06 18:18:03 +0000

    Search bar in map added

--- src/front/js/pages/maps.js
+++ src/front/js/pages/maps.js
@@ -7,6 +7,7 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import { Form } from "react-bootstrap";
 
 let DefaultIcon = L.icon({
 	iconUrl: icon,
@@ -23,8 +24,15 @@ export const Map = () => {
 			<div className="text-center mt-0 d-flex flex-column justify-content-center">
 				<h1 className="mb-3">{"Find your local store"}</h1>
 				<div className="d-flex flex-row">
-					<p className="col-4">
+					<p className="col-4 flex-column">
 						<img className="col-7 d-flex align-items-center" src={planeImageUrl} />
+						<Form className="mt-5">
+							<Form.Control
+								placeholder="Search local shops"
+								// onChange={handleInput}
+								className="bar-body-dropdown col-12 p-3 m-auto mt-5 w-100"
+							/>
+						</Form>
 					</p>
 					<div className="col-md-9 p-3 ">
 						<MapContainer
