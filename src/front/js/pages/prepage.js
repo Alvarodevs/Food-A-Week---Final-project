de78781e19ebf5a9bf0eb44a6efdb0654138bafd
nitlara <nitlara@gmail.com>
2021-03-20 17:16:56 +0000

    reorganitzation prepage items

--- src/front/js/pages/prepage.js
+++ src/front/js/pages/prepage.js
@@ -5,43 +5,39 @@ import HealthyFoodImg from "../../img/eating.png";
 import PhoneImg from "../../img/phone.png";
 import { SignUp } from "../component/signUp";
 import { Login } from "../component/login";
+import { Footer } from "../component/footer";
 import "../../styles/index.scss";
 
 export const Pre = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container-fluid pre-page-container">
-			<div className="container">
-				<div className="text-pre-page">
-					<p className="title-web ml-5">Food A Week</p>
-
-					<p className="pre-text ml-5">Search, Organize and Reuse your work</p>
-					<p className="pre-text ml-5">Make your time more effective & enjoy your food</p>
-				</div>
-				<div className="button-group-pre d-flex flex-column justify-content-end align-items-end">
-					<button className="btn btn-lg green-button">New sign up</button>
-					<SignUp />
-					<button className="btn green-button">Login</button>
-					<Login />
-				</div>
-				<div className="d-flex row">
-					<p className="prepage-text">Search your favorites recipes</p>
-					<img className="col-6 " src={PhoneImg} />
-					<p className="prepage-text">Assaign recipes for every day</p>
-				</div>
-				<div className="d-flex row ">
-					<p className="prepage-text align-item-start">Search your favorites recipes</p>
-					<img className="col-6 " src={HealthyFoodImg} />
-					<p className="prepage-text align-item-end">Use & reuse</p>
-				</div>
-				<div className="d-flex justify-content-center">
-					<p className="prepage-text">& enjoy your time</p>
+		<div className="container-fluid ">
+			<div className="head-prepage d-flex justify-content-between">
+				<div className="title-web ml-5 mr-auto p-2">Food A Week</div>
+				<div className="d-flex flex-column buttons-log">
+					<button className="btn btn-lg green-button  m-0 5 p-1">New sign up</button>
+					<button className="btn btn-lg green-button mt-5 mr-5 p-1">Login</button>
 				</div>
 			</div>
-			{/* <div>
-				{/* <Footer />
-			</div> */}
+			<div className="text-pre-page d-flex flex-column align-items-end ">
+				<p className="prepage-text">Search your favorites recipes</p>
+				<p className="prepage-text">Assaign recipes for every day</p>
+				<p className="prepage-text align-item-end">Use & reuse</p>
+				<img className="img-first col-6 " src={HealthyFoodImg} />
+			</div>
+			<div className="d-flex row ">
+				<img className="col-6 " src={PhoneImg} />
+				<p className="prepage-text">& enjoy your time</p>
+			</div>
+			<div>
+				<SignUp />
+				<Login />
+			</div>
+
+			<div>
+				<Footer />
+			</div>
 		</div>
 	);
 };
