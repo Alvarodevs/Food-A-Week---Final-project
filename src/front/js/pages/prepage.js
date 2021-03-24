eb2583d943ea6da951cdfb767c4ec98f38fb2786
nitlara <nitlara@gmail.com>
2021-03-24 16:38:07 +0000

    collapse element login and sign up

--- src/front/js/pages/prepage.js
+++ src/front/js/pages/prepage.js
@@ -7,59 +7,60 @@ import { Footer } from "../component/footer";
 import "../../styles/index.scss";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Button, Collapse } from "react-bootstrap";
 
 export const Pre = () => {
 	const { store, actions } = useContext(Context);
-
+	const [open, setOpen] = useState(false);
+	const [openLogin, setOpenLogin] = useState(false);
 	return (
 		<div className="container-fluid ">
-			<div className="head-prepage d-flex justify-content-begin">
+			<div className="head-prepage d-flex justify-content-begin d-flex ">
 				<div className="title-web ml-5 mr-auto p-2">Food A Week</div>
-				<div className="d-flex flex-column buttons-log justify-content-end">
-					<button
-						className="btn prebutton green-button mt-1  p-1"
-						type="button"
-						data-toggle="collapse"
-						data-target="#signUpComponent"
-						aria-expanded="false"
-						aria-controls="signUpComponent">
-						New sign up
-					</button>
-					<button
-						className="btn prebutton green-button mt-1  p-1"
-						type="button"
-						data-toggle="collapse"
-						data-target="#logincomponent"
-						aria-expanded="false"
-						aria-controls="logincomponent">
-						Login
-					</button>
-				</div>
 			</div>
-			<div className="text-pre-page d-flex flex-column align-items-end ">
+
+			<div className="img-prepage2 col-2" />
+			<div className=" text-pre-page d-flex flex-column align-items-end ">
 				<p className="prepage-text">Search your favorite recipes</p>
 				<p className="prepage-text">Assign recipes for every day</p>
 				<p className="prepage-text align-item-end">Use & reuse</p>
 				<p className="prepage-text" id="last-prepage-text">
 					& enjoy your time
 				</p>
-				<div className="img-prepage" />
-			</div>
-			<div className="d-flex ">
-				<div className="img-prepage2" />
-			</div>
-			<div className="collapse" id="signUpComponent">
-				<SignUp />
-			</div>
 
-			<div className="collapse" id="loginComponent">
-				<div>
-					<SignIn />
+				<div className="img-prepage " />
+
+				<div className="d-flex flex-column buttons-log justify-content-end ">
+					<Button
+						className="btn prebutton green-button mt-1  p-1"
+						onClick={() => setOpen(!open)}
+						aria-controls="example-collapse-text"
+						aria-expanded={open}>
+						New sign up
+					</Button>
+
+					<Button
+						className="btn prebutton green-button mt-1  p-1"
+						onClick={() => setOpenLogin(!openLogin)}
+						aria-controls="example-collapse-text"
+						aria-expanded={openLogin}>
+						Login
+					</Button>
 				</div>
 			</div>
-
 			<div>
-				<Footer />
+				<Collapse in={open}>
+					<div>
+						<SignUp />
+					</div>
+				</Collapse>
+			</div>
+			<div>
+				<Collapse in={openLogin}>
+					<div>
+						<SignIn />
+					</div>
+				</Collapse>
 			</div>
 		</div>
 	);
