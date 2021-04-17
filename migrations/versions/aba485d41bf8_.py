447b22b840b3be9c445a44534c82c25d144ba32e
nitlara <nitlara@gmail.com>
2021-04-17 15:05:59 +0000

    vuelta a tras de los cambios de filtro de return en weeks

--- migrations/versions/aba485d41bf8_.py
+++ migrations/versions/aba485d41bf8_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 8af42aa118b0
+Revision ID: aba485d41bf8
 Revises: 
-Create Date: 2021-04-17 13:26:04.633195
+Create Date: 2021-04-17 14:56:43.908593
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '8af42aa118b0'
+revision = 'aba485d41bf8'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/front/js/pages/weeks.js
+++ src/front/js/pages/weeks.js
@@ -73,7 +73,6 @@ export const RecipeCard = props => {
 				console.log("delete menu: ", result);
 				toast.info("You have deleted one of your weekly menus");
 				history.push("/home");
-				history.push("/weeks");
 			})
 			.catch(error => error);
 	};
@@ -118,38 +117,18 @@ export const AllWeeks = () => {
 
 	let dayMenu = listOfMenus.map((item, index) => {
 		//fetch to days
-		return;
-		<RecipeCard key={index} id={item.id} title={item.title} />;
+		return <RecipeCard key={index} id={item.id} title={item.title} />;
 	});
-	console.log(dayMenu.length);
-	if (dayMenu.length > 0) {
-		return (
-			<div className="container-fluid">
-				<div className="page-container d-flex">
-					<div className="card-container d-flex justify-content-center mx-auto">
-						<div className="row all-cards ">{dayMenu ? dayMenu : ""}</div>
-					</div>
+
+	return (
+		<div className="container-fluid">
+			<div className="page-container d-flex">
+				<div className="card-container d-flex justify-content-center mx-auto">
+					<div className="row all-cards ">{dayMenu ? dayMenu : ""}</div>
 				</div>
 			</div>
-		);
-	} else {
-		return (
-			<div className="container-fluid">
-				<div className="page-container d-flex flex-column justify-content-center mx-auto pt-5">
-					<div className="card-container mx-auto ">
-						<p className="justify-content-center mx-auto ">
-							<h3>{"You still don't have weekly menus saved"}</h3>
-						</p>
-						<div className="d-flex mx-auto justify-content-center ">
-							<button className="green-button btn p-2" id="button-0menus">
-								<Link to="/new_week">New week</Link>
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+		</div>
+	);
 };
 
 RecipeCard.propTypes = {
