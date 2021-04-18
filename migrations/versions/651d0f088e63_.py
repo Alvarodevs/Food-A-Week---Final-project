17b07c07e8b1b9e0ffe494b1540e5372dd65ddac
nitlara <nitlara@gmail.com>
2021-04-18 17:30:05 +0000

    cambios para incluir en bd selected recipe label

--- migrations/versions/651d0f088e63_.py
+++ migrations/versions/651d0f088e63_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 2c68eafd4e1e
+Revision ID: 651d0f088e63
 Revises: 
-Create Date: 2021-04-18 14:20:56.450544
+Create Date: 2021-04-18 17:19:43.556498
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2c68eafd4e1e'
+revision = '651d0f088e63'
 down_revision = None
 branch_labels = None
 depends_on = None
